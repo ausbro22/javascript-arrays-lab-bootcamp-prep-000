12897f7323973e71bc06eaeac346110b74575c92
Austin Browne <aus.bro22@gmail.com>
2018-02-27 21:04:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+kittens.push(name)
+return kittens
+}
+function destructivelyPrependKitten(name) {
+kittens.unshift(name)
+return kittens
+}
+function destructivelyRemoveLastKitten() {
+kittens.pop()
+return kittens
+}
+function destructivelyRemoveFirstKitten() {
+kittens.shift()
+return kittens
+}
+function appendKitten(name) {
+var newKittens = kittens.push(name)
+return newKittens
+}
+function prependKitten(name) {
+var newKittens = kittens.unshift(name)
+return newKittens
+}
+function removeLastKitten() {
+var newKittens = kittens.pop()
+return newKittens
+}
+function removeFirstKitten(){
+var newKittens = kittens.shift()
+return newKittens
+}
